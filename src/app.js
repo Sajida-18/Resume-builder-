0c020643e56f6b3e038866db1141494583cf8696
Sajida-18 <136361517+Sajida-18@users.noreply.github.com>
2026-08-26 22:03:40 +0600

    Creating auth Routers, controllers and register user implementation

--- src/app.js
+++ src/app.js
@@ -4,4 +4,10 @@ const app = express(); //initialize express app(mainly the server means server i
 
 app.use(express.json());
 
+/* Require all the routes here */
+const authRouter = require("./routes/auth.routes");
+
+/*using all the routes here */
+app.use("/api/auth", authRouter);
+
 module.exports = app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const userModel = reqruire("../models/user.model");
+
+/**
+ *@route registerUserController
+ *@description: Register a new user, expects username, email, and password in the request body
+ *@access Public
+ */
+async function registerUserController(req, res) {}
+
+module.exports = {
+  registerUserController,
+};
